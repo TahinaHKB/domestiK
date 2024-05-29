d1652193deadc0e653f38a4edab2e9e9aa67e9d5
HKB <tahinaandriantsoa2004@gmail.com>
2024-05-29 15:02:17 +0200

    ajout idFoyer dans Tache

--- dom/models.py
+++ dom/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Tache(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.TextField()
-
 class Membre(models.Model):
     nom = models.CharField(max_length = 20)
     prenom = models.CharField(max_length = 30)
@@ -15,5 +11,10 @@ class Foyer(models.Model):
     name = models.CharField(max_length=20)
     member = models.ManyToManyField(Membre)
     admin = models.IntegerField()
+
+class Tache(models.Model):
+    name = models.CharField(max_length=20)
+    foyer = models.ForeignKey(Foyer, on_delete=models.CASCADE)
+    description = models.TextField()
     
    
