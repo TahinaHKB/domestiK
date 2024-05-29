1db9e447afe08dc2946b906f6d04ba8a4773a6a3
HKB <tahinaandriantsoa2004@gmail.com>
2024-05-29 14:33:58 +0200

    Ajout prenom

--- dom/models.py
+++ dom/models.py
@@ -7,6 +7,7 @@ class Tache(models.Model):
 
 class Membre(models.Model):
     nom = models.CharField(max_length = 20)
+    prenom = models.CharField(max_length = 30)
     email = models.EmailField(max_length = 50)
     mot_de_passe = models.CharField(max_length = 30)
 
